b6b3aabfc9253bf99d0685632c2e5f0f27230589
BryceMarshall <bryce.o.marshall@gmail.com>
2014-09-27 16:18:27 -0400

    Revert "Almost-working search page"
    This reverts commit 0f43bd2ea95ffa5fb99e26da76e73979c16b308a.

--- QExchange/src/com/example/qexchange/SearchResultPage.java
+++ QExchange/src/com/example/qexchange/SearchResultPage.java
@@ -1,49 +1,15 @@
 package com.example.qexchange;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.ListActivity;
-import android.app.SearchManager;
-import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CursorAdapter;
-import android.widget.SimpleCursorAdapter;
 
-public class SearchResultPage extends ListActivity 
-		implements LoaderManager.LoaderCallback<Cursor>{
-	
-	SimpleCursorAdapter mAdapter;
+public class SearchResultPage extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search_result_page);
-		
-		
-		String[] fromColumns = {};
-	    int[] toViews = {android.R.id.text1};
-	        
-	        
-		mAdapter = new SimpleCursorAdapter(this, 
-                android.R.layout.simple_list_item_1, null,
-                fromColumns, toViews, 0);
-        setListAdapter(mAdapter);
-        
-       
-	
-			Intent intent = getIntent();
-			if (Intent.ACTION_SEARCH.equals(intent.getAction())){
-				String query = intent.getStringExtra(SearchManager.QUERY);
-				displayResults(query);
-			}
 	}
 
 	@Override
@@ -52,11 +18,5 @@ public class SearchResultPage extends ListActivity
 		getMenuInflater().inflate(R.menu.search_result_page, menu);
 		return true;
 	}
-	
-	public void displayResults(String query){
-		
-	}
-	
-	
 
 }
