661e3b2d6a3267c175b46405bb4d5dfbee4fee69
BryceMarshall <bryce.o.marshall@gmail.com>
2014-09-27 16:26:06 -0400

    Revert "Almost working search"
    This reverts commit beb40d9eb741ebab4ca8737b1e3a51783b231167.

--- QExchange/src/com/example/qexchange/AddBookPage.java
+++ QExchange/src/com/example/qexchange/AddBookPage.java
@@ -1,15 +1,28 @@
 package com.example.qexchange;
 
+import com.mysql.jdbc.Statement;
+
+import android.os.Build;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class AddBookPage extends Activity {
+	DatabaseQuery database = new DatabaseQuery();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_book_page);
+		if (Build.VERSION.SDK_INT >=Build.VERSION_CODES.HONEYCOMB){
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+		String st = "INSERT INTO Accounts (ID, email, password, name) values ('5','foger','test','mac')";
+		database.execute(st);
+		
+		//database.insert(st);
+
 	}
 
 	@Override
@@ -18,5 +31,7 @@ public class AddBookPage extends Activity {
 		getMenuInflater().inflate(R.menu.book_page, menu);
 		return true;
 	}
+
+
 
 }
--- QExchange/src/com/example/qexchange/MainPage.java
+++ QExchange/src/com/example/qexchange/MainPage.java
@@ -2,10 +2,7 @@ package com.example.qexchange;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.SearchManager;
-import android.content.Context;
 import android.view.Menu;
-import android.widget.SearchView;
 
 public class MainPage extends Activity {
 
@@ -19,13 +16,6 @@ public class MainPage extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_page, menu);
-		
-//		 SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-//		    SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
-//		    // Assumes current activity is the searchable activity
-//		    searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-//		    searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
-		    
 		return true;
 	}
 
