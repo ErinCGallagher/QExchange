beb40d9eb741ebab4ca8737b1e3a51783b231167
BryceMarshall <bryce.o.marshall@gmail.com>
2014-09-27 16:23:09 -0400

    Almost working search

--- QExchange/src/com/example/qexchange/LoginPage.java
+++ QExchange/src/com/example/qexchange/LoginPage.java
@@ -6,19 +6,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
 public class LoginPage extends Activity {
-	
-	EditText usernameField, passwordField;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login_page);
-		usernameField = (EditText)findViewById(R.id.userField);
-		passwordField = (EditText)findViewById(R.id.passField);
 	}
 
 	@Override
@@ -27,22 +21,5 @@ public class LoginPage extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	public void launchCreateAccountPage(View view){
-		startActivity(new Intent(LoginPage.this, CreateAccountPage.class));
-    }
-	
-	public void submitLogin(View view){
-		
-		//validate login (query table
-		//if valid, go to main page, else stay here, show error
-		if (usernameField.getText().toString().equals("") || passwordField.getText().toString().equals("")){
-			Toast toast = Toast.makeText(getApplicationContext(), "Please enter username and password", Toast.LENGTH_LONG);
-			toast.show();
-		} else {
-			//create account: add appropriate info to tables, check that email not already there
-			startActivity(new Intent(LoginPage.this, MainPage.class));
-		}
-    }
 
 }
