0f43bd2ea95ffa5fb99e26da76e73979c16b308a
BryceMarshall <bryce.o.marshall@gmail.com>
2014-09-27 16:12:00 -0400

    Almost-working search page
    search page does not work

--- QExchange/src/com/example/qexchange/CreateAccount.java
+++ QExchange/src/com/example/qexchange/CreateAccount.java
@@ -4,18 +4,18 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class AddBookPage extends Activity {
+public class CreateAccount extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_add_book_page);
+		setContentView(R.layout.activity_create_account);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.book_page, menu);
+		getMenuInflater().inflate(R.menu.create_account, menu);
 		return true;
 	}
 
