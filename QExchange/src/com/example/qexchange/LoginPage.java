b52390f515356aa7db336f8b9d507b7820ad1ed7
90ne1 <mac.stewart@live.ca>
2014-09-28 00:47:28 -0400

    Added login functionality with server verification

--- QExchange/src/com/example/qexchange/LoginPage.java
+++ QExchange/src/com/example/qexchange/LoginPage.java
@@ -1,6 +1,11 @@
 package com.example.qexchange;
 
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.concurrent.ExecutionException;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -38,11 +43,36 @@ public class LoginPage extends Activity {
 		//if valid, go to main page, else stay here, show error
 		String emailInput = usernameField.getText().toString();
 		String passwordInput = passwordField.getText().toString();
+		Account userAccount;
 		if (emailInput.length() <= 0 || passwordInput.length() < 5){
 			Toast toast = Toast.makeText(getApplicationContext(), "Please enter valid username and password", Toast.LENGTH_LONG);
 			toast.show();
 		} else {
-			
+			String query = "SELECT * FROM Accounts WHERE email = '"+emailInput+"' AND password = '"+passwordInput+"'";
+			System.out.println(query);
+			try {
+				ResultSet results = Query.query("SELECT", query);
+				System.out.println("Done query");
+				if (results.next()) {
+					System.out.println("In if");
+					ResultSetMetaData rsmd=results.getMetaData();
+
+					String userEmail = results.getString("email");
+					String userPassword = results.getString("password");
+					String userName = results.getString("name");
+					userAccount = new Account(userEmail, userPassword, userName);
+					System.out.println("logged in");
+				} else {
+					Toast toast = Toast.makeText(getApplicationContext(), "Please enter valid username and password", Toast.LENGTH_LONG);
+					toast.show();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
 			//create account: add appropriate info to tables, check that email not already there
 			//startActivity(new Intent(LoginPage.this, MainPage.class));
 		}
