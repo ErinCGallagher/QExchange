a409424e76e499f3a71a9bf95de84afbec384903
BryceMarshall <bryce.o.marshall@gmail.com>
2014-09-27 21:40:55 -0400

    Partial List layout

--- QExchange/src/com/example/qexchange/SearchResultPage.java
+++ QExchange/src/com/example/qexchange/SearchResultPage.java
@@ -1,15 +1,38 @@
 package com.example.qexchange;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
-public class SearchResultPage extends Activity {
+
+public class SearchResultPage extends ListActivity  {
+	
+	String title,author,course, edition,price;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search_result_page);
+
+			Intent intent = getIntent();
+			if (Intent.ACTION_SEARCH.equals(intent.getAction())){
+				String query = intent.getStringExtra(SearchManager.QUERY);
+	//			searchDatabase(query); TODO
+//				displayResults(ResultSet) TODO
+			}
 	}
 
 	@Override
@@ -18,5 +41,23 @@ public class SearchResultPage extends Activity {
 		getMenuInflater().inflate(R.menu.search_result_page, menu);
 		return true;
 	}
+	
+	public void searchDatabase(String query){
+		//TODO
+	}
+	
+	/*public void displayResults(String[] ResultSet){
+		title= ResultSet[0];
+		author= ResultSet[1];
+		edition= (String)ResultSet[2];
+		course = ResultSet[3];
+		price = (String)ResultSet[4];
+	}*/
+	
+	public void displayResults(ArrayList<Book> list){
+		
+	}
+	
+	
 
 }
